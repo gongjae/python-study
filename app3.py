d0a929f035488035a9a70bfec99fe6537d7053a3
gongjae <rudddong@gmail.com>
2023-08-20 14:33:50 +0900

    웹 크롤러 1-2

--- app3.py
+++ app3.py
@@ -6,3 +6,9 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup(데이터.content, 'html.parser')
 print(soup.find_all('strong',id="_nowVal")[0].text)
 print(soup.find_all('span',class_="tah")[4].text)
+soup.select('.tah') 
+soup.select('#tah') 
+soup.select('.f_down em')
+이미지 = soup.select('#img_chart_area')
+print(이미지['src'])
+
