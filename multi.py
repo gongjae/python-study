02ae14cf5104f65bba31f568e4d92123ed699e8e
gongjae <rudddong@gmail.com>
2023-08-26 14:56:08 +0900

    파이썬 멀티쓰레딩

---
+++ multi.py
@@ -0,0 +1,36 @@
+#멀티 프로세싱(여러개의 파이썬 실행창 띄우기)/멀티 쓰레딩(cpu 병렬처리)
+import requests
+import json
+import time
+
+url = [
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1609524000000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1608811200000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1608098400000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1606672800000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1605960000000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1605242700000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1604534400000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1603821600000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1603108800000',
+  'https://tb.coinone.co.kr/api/v1/chart/olhc/?site=coinoneeth&type=1h&last_time=1602396000000',
+]
+
+def 함수(구멍):
+  data = requests.get(구멍)
+  딕셔너리 = json.loads(data.content)
+  return 딕셔너리['data'][0]['Close']
+
+#
+
+
+#멀티 쓰레딩으로 코드 실행시키는 법
+from multiprocessing.dummy import Pool as ThreadPool
+
+pool = ThreadPool(4)
+result = pool.map(함수,url)
+pool.close()
+pool.join()
+print(result)
+
+#
